32ab79bb881c0e2c2702a1493ba49a212947c312
Ivan Valera <ivanvaleraa@gmail.com>
2023-08-27 16:55:17 -0400

    first commit, game finished

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import './App.css'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinnerFrom, checkEndGame } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const updateBoard = (index) => {
+    if(board[index] || winner) return
+
+    const updatedBoard = [...board]
+    updatedBoard[index] = turn
+    setBoard(updatedBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X; 
+    setTurn(newTurn)
+    
+    const newWinner = checkWinnerFrom(updatedBoard)
+    if(newWinner){
+      confetti()
+      setWinner(newWinner)
+    }else if(checkEndGame(updatedBoard)){
+      setWinner(false)
+    }
+  }
+
+  
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+            <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+            >
+             { board[index] }
+            </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame}/>
+    </main>
+  )
+}
+
+export default App
