6dbf5feab71509561a7d30b63f91ce546c80cc3c
Ivan Valera <ivanvaleraa@gmail.com>
2023-08-27 17:20:57 -0400

    save progress with local storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,26 @@ import { checkWinnerFrom, checkEndGame } from './logic/board'
 import { WinnerModal } from './components/WinnerModal'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() =>{
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(()=>{
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
+
   const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const updateBoard = (index) => {
@@ -27,6 +39,9 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X; 
     setTurn(newTurn)
     
+    window.localStorage.setItem('board',JSON.stringify(updatedBoard))
+    window.localStorage.setItem('turn',newTurn)
+
     const newWinner = checkWinnerFrom(updatedBoard)
     if(newWinner){
       confetti()
